c42eab3ec32af8fc6139a2ab957d84dd2eb2cbe4
wyswill <www.1151373571@live.com>
2024-12-09 18:31:21 +0800

    asfd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+use actix::{Actor, Addr};
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
+use actix_web::middleware::{from_fn, Compress, Logger};
+use env::dotenv;
+use once_cell::sync::OnceCell;
+use rbatis::RBatis;
+use rbdc_mysql::MysqlDriver;
+use rs_service_util::{self, jwt::JWT, redis::RedisActor};
+use utoipa::OpenApi;
+use utoipa_actix_web::AppExt;
+use utoipa_scalar::{Scalar, Servable};
+
+mod entity;
+
+#[derive(OpenApi)]
+#[openapi(
+    tags(
+        (name = "store", description = "store 接口"),
+    ),
+    modifiers(&JWT),
+    security(
+        ("JWT" = ["edit:items", "read:items"])
+    )
+)]
+struct ApiDoc;
+
+lazy_static::lazy_static! {
+    static ref REDIS_KEY:String = "store_service".to_string();
+    static ref RB:RBatis=RBatis::new();
+    static ref REDIS_ADDR: OnceCell<Addr<RedisActor>> = OnceCell::new();
+}
+
+#[actix_web::main]
+async fn main() {
+    dotenv().expect("Failed to load .env file");
+    env_logger::init();
+    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
+    let actor = RedisActor::new(redis_url).await;
+    let addr: Addr<RedisActor> = actor.start();
+
+    init_db().await;
+
+    REDIS_ADDR.set(addr.clone()).expect("set redis addr error");
+
+    let _ = HttpServer::new(move || {
+        App::new()
+            .into_utoipa_app()
+            .openapi(ApiDoc::openapi())
+            // .service(utoipa_actix_web::scope("/api/user").configure(user::configure()))
+            .openapi_service(|api| Scalar::with_url("/doc", api))
+            .into_app()
+            .wrap(
+                Cors::default()
+                    .allow_any_origin()
+                    .allowed_methods(vec!["GET", "POST", "DELETE", "PUT", "OPTION"])
+                    .allowed_headers(vec![
+                        http::header::AUTHORIZATION,
+                        http::header::ACCEPT,
+                        http::header::CONTENT_TYPE,
+                    ]),
+            )
+            .wrap(Compress::default())
+            .wrap(Logger::default())
+            .wrap(Logger::new("%a %{Referer}i"))
+        // .wrap(from_fn(jwt_mw))
+    })
+    .keep_alive(None)
+    .shutdown_timeout(5)
+    .bind(gen_server_url())
+    .expect("服务启动失败")
+    .run()
+    .await;
+}
+fn gen_server_url() -> String {
+    let host = "0.0.0.0";
+    let url = format!("{}:{}", host, 3000);
+    log::info!("server is on, addr http://127.0.0.1:3000\n doc:  http://127.0.0.1:3000/doc");
+    url
+}
+
+async fn init_db() {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    log::info!("database_url {database_url}");
+    if let Err(e) = RB.link(MysqlDriver {}, &database_url).await {
+        panic!("db err: {}", e.to_string());
+    }
 }
